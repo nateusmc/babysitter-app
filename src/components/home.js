0b20040dcf5122d83098a4acf151a3615eabf54a
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-27 13:25:52 -0600

    removed app files that came with create-react-app and setup basic homepage component structure

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
