3160ec4f64f2c7ec275b495416bbe8a9500d8557
Adam Bradbury <f.adambrad@gmail.com>
2018-02-08 11:30:14 -0700

    laid out rough draft for new landingpage

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { toggleView } from '../../actions/views';
+import { Redirect } from 'react-router-dom';
+
+import Footer from './Footer';
+
+import './LandingPage.css';
+
+export class LandingPage extends Component {
+    componentDidMount() {
+        this.props.dispatch(toggleView('landing-page'));
+    }
+
+    render(){
+        // need conditional to redirect to parent dash || sitter dash....
+
+        // if(this.props.loggedIn){
+        //     if(this.props.user.role === 'parent'){
+        //         return <Redirect to='parent dashboard endpoint TBD'
+        //     }
+        //     else if(this.props.user.role === 'sitter'){
+        //         return <Redirect to ='sitter dashboard endpoint TBD'
+        //     }
+        // }
+
+        return(
+            <div className="landingPageContainer">
+                <div className="mainTagline">Find you next sitter or family!
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+ }
+
+ const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null,
+    user: state.auth.currentUser,
+  })
+  
+  export default connect(mapStateToProps)(LandingPage);
